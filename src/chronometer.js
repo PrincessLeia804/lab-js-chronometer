10e4bd2976b3f6eff310736923f99a4a9a25d350
Lea Model <lea.skywalker309@gmail.com>
2023-06-20 16:04:33 +0200

    Iteration 4 finished

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,18 +1,34 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      //WHY does it only work with the conditional statement?
+      if (printTimeCallback){
+        printTimeCallback();
+      }
+    }, 1000);
+    
   }
 
   getMinutes() {
     // ... your code goes here
-  }
+      return Math.floor(this.currentTime / 60);
+    }
 
   getSeconds() {
     // ... your code goes here
+    if (!this.currentTime){
+      return 0;``
+    } else {
+      return this.currentTime%60;
+    }
   }
 
   computeTwoDigitNumber(value) {
