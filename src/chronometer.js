36fd99ba96903fccd428eab469d4c5922159ccd9
Lea Model <lea.skywalker309@gmail.com>
2023-06-20 16:19:32 +0200

    Iteration 7 finished

--- src/chronometer.js
+++ src/chronometer.js
@@ -43,10 +43,12 @@ class Chronometer {
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
